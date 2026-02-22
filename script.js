287681dcfc5ef82a4a1b58331ac2b3e53dd36bd4
aamiraaaa <tassybayamira@gmail.com>
2026-02-22 15:07:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-// Starter JS
-// Steps for students to implement:
-// 1. Get user inputs
-// 2. Perform calculations
-// 3. Use if/else to classify results
-// 4. Display results in #output
+function calculateSavings() {
+
+    let income = parseFloat(document.getElementById("income").value);
+    let percent = parseFloat(document.getElementById("percent").value);
+    let goal = parseFloat(document.getElementById("goal").value);
+
+    if (!income || !percent || !goal) {
+        document.getElementById("result").innerText = "Please enter valid numbers.";
+        return;
+    }
+
+    let monthlySavings = income * (percent / 100);
+
+    let monthsNeeded = Math.ceil(goal / monthlySavings);
+
+    document.getElementById("result").innerHTML =
+        "You save ₸" + monthlySavings.toFixed(2) + " per month.<br>" +
+        "To reach ₸" + goal + " you need about " + monthsNeeded + " months.";
+}
